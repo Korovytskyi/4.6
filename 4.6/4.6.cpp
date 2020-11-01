d673436adc8554f40b40ff877df1d074a7f0ef11
Taras <taras.korovytskyi.itis.2020@lpnu.ua>
2020-11-01 12:46:24 +0200

    4.6

---
+++ 4.6/4.6.cpp
@@ -0,0 +1,61 @@
+﻿// 4.6.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double S1, S2;
+	int k, i;
+	S1 = 0;
+	k = 1;
+	while (k <= 20)
+	{
+		S2 = 0;
+		i = k;
+		while (i <= 40-k)
+		{
+			S2 +=i*i;
+			i++;
+		}
+		S1 += 1+(sqrt(S2) / (k*k));
+		k++;
+	}
+	cout << S1 << endl;
+	S1 = 0;
+	k = 1;
+	do {
+		S2 = 0;
+		i = k;
+		do {
+			S2 += i*i;
+			i++;
+		} while (i <= 40-k);
+		S1 += 1+(sqrt(S2) / (k*k));
+		k++;
+	} while (k <= 20);
+	cout << S1 << endl;
+	S2 = 0;
+	for (k = 1; k <= 20; k++)
+	{
+		S2 = 0;
+		for (i = k; i <= 40-k; i++)
+		{
+			S2 += i*i;
+		}
+		S1 += 1 + (sqrt(S2) / (k * k));
+	}
+	cout << S1 << endl;
+	S1 = 0;
+	for (k = 20; k >= 1; k--)
+	{
+		S1 = 0;
+		for (i = 40-k; i >= 1; i--)
+		{
+			S2 += (i * i);
+			
+		}
+		S1 += 1 + (sqrt(S2) / (k * k));
+	}
+	cout << S1 << endl;
+	return 0;
+}
